b802042fd5e420eeb8781f4caefd9a727453c012
Elena Prekrasnaya <elenaprekrasnaya312@gmail.com>
2023-12-22 17:03:08 +0400

    add ДЗ:DZ

---
+++ OneDrive/Рабочий стол/дз/DZ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задайте значения для a, b, c и d
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+
+        // Проверяем, что знаменатель не равен нулю
+        if (c + d != 0)
+        {
+            // Вычисляем значение формулы
+            double result = (double)(a * b) / (c + d);
+
+            // Выводим результат на экран
+            Console.WriteLine("Результат: " + result);
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: деление на ноль");
+        }
+    }
+}
